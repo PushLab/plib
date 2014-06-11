3f50a12cc517dd82a755f573c7ff868414884972
Push Chen <littlepush+github@gmail.com>
2014-06-12 00:15:04 +0800

    update code

--- Test/pdns.cpp
+++ Test/pdns.cpp
@@ -14,6 +14,152 @@ using namespace Plib::Network;
 using namespace Plib::Utility;
 using namespace std;
 
+struct TBlackListNode;
+typedef pair< String, TBlackListNode * >   TBlackItem;
+struct TBlackListNode
+{
+    Array<TBlackItem>               prefixList;
+    Array<TBlackItem>               suffixList;
+    Array<TBlackItem>               containList;
+    map<String, TBlackListNode *>   keys;
+    TBlackListNode                  *everything;
+    operator bool() const 
+    {
+        return prefixList.Size() != 0 || suffixList.Size() != 0 || 
+            containList.Size() != 0 || keys.size() != 0 || everything != NULL;
+    }
+    TBlackListNode() : everything(NULL) {}
+};
+
+ostream & operator << ( ostream & os, const TBlackItem &item );
+ostream & operator << ( ostream & os, const TBlackListNode &node );
+
+ostream & operator << ( ostream & os, const TBlackItem &item )
+{
+    os << item.first << ": {" << *item.second << "}" << endl;
+    return os;
+}
+ostream & operator << ( ostream & os, const TBlackListNode &node )
+{
+    if ( node == false ) {
+        os << "LeafNode";
+    } else {
+        if ( node.prefixList.Size() ) {
+            os << "Prefix:" << endl;
+            for ( int i = 0; i < node.prefixList.Size(); ++i ) {
+                os << node.prefixList[i];
+            }
+        }
+        if ( node.suffixList.Size() ) {
+            os << "Suffix:" << endl;
+            for ( int i = 0; i < node.suffixList.Size(); ++i ) {
+                os << node.suffixList[i];
+            }
+        }
+        if ( node.containList.Size() ) {
+            os << "Contain:" << endl;
+            for ( int i = 0; i < node.containList.Size(); ++i ) {
+                os << node.containList[i];
+            }
+        }
+        if ( node.keys.size() ) {
+            for ( map<String, TBlackListNode *>::const_iterator _it = node.keys.begin();
+                _it != node.keys.end(); ++_it )
+            {
+                os << _it->first << ": " << *_it->second;
+            }
+        }
+    }
+    return os;
+}
+
+TBlackListNode       *gBlRoot = NULL;
+
+void addPatternToBlackList( String pattern ) 
+{
+    if ( gBlRoot == NULL ) {
+        gBlRoot = new TBlackListNode;
+    }
+    TBlackListNode *_blNode = gBlRoot;
+    Array<String> _components = pattern.Split(".");
+    for ( int i = _components.Size() - 1; i >= 0; --i ) {
+        String _com = _components[i];
+        PINFO("Parsing component: " << _com);
+        bool _beginWithPattern = _com[0] == '*';
+        bool _endWithPattern = _com[_com.Size() - 1] == '*';
+        if ( _com == "*" ) {
+            PINFO( _com << " stands for everything..." );
+            _blNode->everything = new TBlackListNode;
+            _blNode = _blNode->everything;
+            continue;
+        }
+        if ( _beginWithPattern == false && _endWithPattern == false ) {
+            PINFO(_com << " is a static key");
+            if ( _blNode->keys.find(_com) == _blNode->keys.end() ) {
+                _blNode->keys[_com] = new TBlackListNode;
+            } 
+            _blNode = _blNode->keys[_com];
+        } else if ( _beginWithPattern == true && _endWithPattern == true ) {
+            PINFO(_com << " has both patten.");
+            bool _hasSameKey = false;
+            String _ckey = _com.SubString(1, _com.Size() - 2);
+            PINFO("static part is: " << _ckey);
+            for ( int c = 0; c < _blNode->containList.Size(); ++c ) {
+                TBlackItem _item = _blNode->containList[c];
+                if ( _item.first == _ckey ) {
+                    _hasSameKey = true;
+                    _blNode = _item.second;
+                    break;
+                }
+            }
+            if ( !_hasSameKey ) {
+                TBlackListNode *_nextLevelNode = new TBlackListNode;
+                TBlackItem _newItem = make_pair(_ckey, _nextLevelNode);
+                _blNode->containList.PushBack(_newItem);
+                _blNode = _nextLevelNode;
+            }
+        } else if ( _beginWithPattern == true && _endWithPattern == false ) {
+            PINFO(_com << " is a suffix.");
+            bool _hasSameKey = false;
+            String _ckey = _com.SubString(1);
+            PINFO("static part is: " << _ckey);
+            for ( int c = 0; c < _blNode->suffixList.Size(); ++c ) {
+                TBlackItem _item = _blNode->suffixList[c];
+                if ( _item.first == _ckey ) {
+                    _hasSameKey = true;
+                    _blNode = _item.second;
+                    break;
+                }
+            }
+            if ( !_hasSameKey ) {
+                TBlackListNode *_nextLevelNode = new TBlackListNode;
+                TBlackItem _newItem = make_pair(_ckey, _nextLevelNode);
+                _blNode->suffixList.PushBack(_newItem);
+                _blNode = _nextLevelNode;
+            }
+        } else if ( _beginWithPattern == false && _endWithPattern == true ) {
+            PINFO(_com << " is a prefix.");
+            bool _hasSameKey = false;
+            String _ckey = _com.SubString(0, _com.Size() - 1);
+            PINFO("static part is: " << _ckey);
+            for ( int c = 0; c < _blNode->prefixList.Size(); ++c ) {
+                TBlackItem _item = _blNode->prefixList[c];
+                if ( _item.first == _ckey ) {
+                    _hasSameKey = true;
+                    _blNode = _item.second;
+                    break;
+                }
+            }
+            if ( !_hasSameKey ) {
+                TBlackListNode *_nextLevelNode = new TBlackListNode;
+                TBlackItem _newItem = make_pair(_ckey, _nextLevelNode);
+                _blNode->prefixList.PushBack(_newItem);
+                _blNode = _nextLevelNode;
+            }
+        }
+    }
+}
+
 #pragma pack(push, 1)
 struct dnsPackage {
     Uint16          transactionId;
@@ -39,8 +185,20 @@ SOCKET_T            gSvrSockUdp;
 
 bool isDomainInBlacklist( Array<String> domainComs )
 {
-
-	return false;
+    TBlackListNode *_blNode = gBlRoot;
+    for ( int i = domainComs.Size() - 1; i >= 0; --i ) {
+        String _com = domainComs[i];
+        if ( _blNode->keys.find(_com) != _blNode->keys.end() ) {
+            _blNode = _blNode->keys[_com];
+            continue;
+        }
+        // Try to search suffix
+        // Try to search prefix
+        // Try to search contain
+        // If contains [everything]
+    }
+    // If the node is leaf node, means match.
+	return (*_blNode) == false;
 }
 
 void getTcpConnection()
@@ -155,21 +313,89 @@ void getUdpConnection()
     _ssvr.Close();
 }
 
+void pdnsVersionInfo()
+{
+    printf( "pdns\tversion 1.0\n" );
+}
+
+void pdnsHelpInfo()
+{
+    printf( "pdns -h|--help\n" );
+    printf( "pdns -v|--version\n" );
+    printf( "pdns [-b|--blacklist <path>] [-ld|--localdns <dns>] [-bd|--blackdns <dns>]\n");
+}
+
 int main( int argc, char * argv[] ) {
 
 	// Try to parse the command line argument
-	String _blackListFilePath = "~/.pdns.baacklist";
+	String _blackListFilePath = "~/.pdns.blacklist";
 	String _localParentDns = "202.96.209.5";
 	String _blackParentDns = "8.8.8.8";
 
 	if ( argc >= 2 ) {
-
+        int _arg = 1;
+        for ( ; _arg < argc; ++_arg ) {
+            String _command = argv[_arg];
+            if ( _command == "-h" || _command == "--help" ) {
+                // Help
+                pdnsHelpInfo();
+                return 0;
+            }
+            if ( _command == "-v" || _command == "--version" ) {
+                // Version
+                pdnsVersionInfo();
+                return 0;
+            }
+            if ( _command == "-b" || _command == "--blacklist" ) {
+                if ( _arg + 1 < argc ) {
+                    _blackListFilePath = argv[++_arg];
+                } else {
+                    cerr << "Invalidate argument: " << _command << ", missing parameter." << endl;
+                    return 1;
+                }
+                continue;
+            }
+            if ( _command == "-ld" || _command == "--localdns" ) {
+                if ( _arg + 1 < argc ) {
+                    _localParentDns = argv[++_arg];
+                } else {
+                    cerr << "Invalidate argument: " << _command << ", missing parameter." << endl;
+                    return 1;
+                }
+                continue;
+            }
+            if ( _command == "-bd" || _command == "--blackdns" ) {
+                if ( _arg + 1 < argc ) {
+                    _blackParentDns = argv[++_arg];
+                } else {
+                    cerr << "Invalidate argument: " << _command << ", missing parameter." << endl;
+                    return 1;
+                }
+                continue;
+            }
+            cerr << "Invalidate argument: " << _command << "." << endl;
+            return 1;
+        }
 	}
+
+    ReadStream _blStream(_blackListFilePath);
+    if ( !_blStream ) {
+        cerr << "Failed to open black list file at path: " << _blackListFilePath << endl;
+        cerr << "All dns query will be route to the local dns server." << endl;
+    } else {
+        String _configLine = _blStream.ReadLine();
+        for ( ; _configLine != String::Null; _configLine = _blStream.ReadLine() ) {
+            cout << _configLine << endl;
+            // Try to build the search tree.
+            addPatternToBlackList( _configLine );
+        }
+        cout << *gBlRoot << endl;
+    }
 
     // Wait for exit
     SetSignalHandle();
 
-    Uint32 _svrPort = 53;
+    Uint32 _svrPort = 1053;
     // Start to build the socket for both tcp and udp
     struct sockaddr_in _sockAddrTcp, _sockAddrUdp;
 
